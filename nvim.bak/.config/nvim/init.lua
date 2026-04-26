4e2a72b410d03f4224a86eeca43c9272864d5787
Lyxsly <lyxs7y@gmail.com>
2026-04-26 22:42:55 +0900

    Add lazyvim and zsh,wezterm

---
+++ nvim.bak/.config/nvim/init.lua
@@ -0,0 +1,24 @@
+-- share clipboard with OS
+vim.opt.clipboard:append('unnamedplus,unnamed')
+
+vim.opt.expandtab = true
+vim.opt.shiftround = true
+vim.opt.shiftwidth = 2
+vim.opt.softtabstop = 2
+vim.opt.tabstop = 2
+
+vim.opt.scrolloff = 3
+
+vim.opt.whichwrap = 'b,s,h,l,<,>,[,],~'
+
+vim.opt.relativenumber = true
+vim.opt.cursorline = true
+
+vim.api.nvim_create_user_command(
+  'InitLua',
+  function()
+    vim.cmd.edit(vim.fn.stdpath('config')..'/init.lua')
+  end,
+  {desc = 'Open init.lua'}
+)
+
--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,27 +1,65 @@
 -- WeztermのAPIを読み込む
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 local act = wezterm.action
+config.automatically_reload_config = true
 
 -- 🎨 1. カラースキーム（テーマ）の設定
--- Weztermには最初から数百種類のテーマが入っています。人気の暗いテーマを指定。
-config.color_scheme = 'Tokyo Night'
+-- config.color_scheme = "Tokyo Night"
 
--- 🔤 2. フォントの設定（先ほど入れた魔法のフォント）
-config.font = wezterm.font 'JetBrainsMono Nerd Font'
+config.font = wezterm.font("JetBrainsMono Nerd Font")
 config.font_size = 12.0
+config.use_ime = true
 
 -- 👻 3. 背景の透過（0.0が完全透明、1.0が不透明）
 config.window_background_opacity = 0.85
-config.text_background_opacity = 1.0 -- 文字の背景は透かさない（読みやすさ重視）
+config.macos_window_background_blur = 20
+config.text_background_opacity = 1.0
+config.window_decorations = "RESIZE"
+config.hide_tab_bar_if_only_one_tab = true
 
+config.window_frame = {
+	inactive_titlebar_bg = "none",
+	active_titlebar_bg = "none",
+}
+
+config.window_background_gradient = {
+	colors = { "#000000" },
+}
+
+config.show_new_tab_button_in_tab_bar = false
+config.show_close_tab_button_in_tabs = false
+
+config.colors = {
+	tab_bar = {
+		inactive_tab_edge = "none",
+	},
+}
+
+wezterm.on("format-tab-titel", function(tab, tabs, panes, config, hover, max_width)
+	local background = "#5c6d74"
+	local foreground = "#FFFFFF"
+
+	if tab.is_active then
+		background = "#ae8b2d"
+		foreground = "#FFFFFF"
+	end
+
+	local title = "   " .. wezterm.truncate_right(tab.active_pame, max_width - 1) .. "   "
+
+	return {
+		{ Background = { Color = background } },
+		{ Foreground = { Color = foreground } },
+		{ Text = title },
+	}
+end)
 -- 🖼️ 4. 見た目の微調整
-config.hide_tab_bar_if_only_one_tab = true -- タブが1つの時はバーを隠してスッキリさせる
+config.hide_tab_bar_if_only_one_tab = true
 config.window_padding = {
-  left = 10,
-  right = 10,
-  top = 10,
-  bottom = 10,
+	left = 10,
+	right = 10,
+	top = 10,
+	bottom = 10,
 }
 --config.enable_wayland=false
 
